512cfcad83a9a6209daaf65cff17b19a7dcbf872
Joseph VonDoloski <vondoloskijd@masters.edu>
2022-02-01 18:46:51 -0800

    Added fix

--- source/CardGames.py
+++ source/CardGames.py
@@ -106,7 +106,7 @@ class Player:
 
   def highCard(self):
     curr_high=self.hand[0]
-    for i in range(len(self.hand-1):
+    for i in range(len(self.hand):
        if curr_high.value < self.hand[i].value:
            curr_high=self.hand[i]
     return curr_high
