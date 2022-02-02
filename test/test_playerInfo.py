f69e5d07219630b3292310335d2b8e6e2c152182
Joanna Johnson <johnsonjs1@masters.edu>
2022-02-01 18:00:28 -0800

    Trial Sprint 1

---
+++ test/test_playerInfo.py
@@ -0,0 +1,7 @@
+from testing_base import *
+
+def PlayerDetails():
+    #testPlayer = Player("Joanna")
+    #testPlayer = Player()
+    #return "Player's name is % s, known cards are % s, and money is % d." % (testPlayer.name, testPlayer.knownCards, testPlayer.money)
+    return PlayerList
