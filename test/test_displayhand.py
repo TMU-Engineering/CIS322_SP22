268042295512edbefe5d3c259dc2d5f2d214c628
Ryan R <rastguelenianrn@masters.edu>
2022-02-12 15:55:21 -0800

    review

---
+++ test/test_displayhand.py
@@ -0,0 +1,22 @@
+from testing_base import *
+
+def test_something():
+
+  
+  deck1 = Deck()
+  player1 = Player("john")
+  player2 = Player("jeff")
+  player3 = Player("george")
+  players2 = [player1, player2, player3]
+  dealer1 = Dealer()
+
+
+
+ # dealer1.dealCards(3, [player2], deck1)
+ # dealer1.dealCards(4, [player3], deck1)
+ # dealer1.printAllPlayerCards_test(players2)
+
+
+  
+
+
