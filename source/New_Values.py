598e47ce07c9fb430730852a2c8b0f3bf75eb131
Jacob McMillan <mcmillanjy@masters.edu>
2022-03-01 20:51:52 -0800

    Updated code

--- source/New_Values.py
+++ source/New_Values.py
@@ -1,15 +1,17 @@
 from CardGames import *
-def face_values(deck):
-  for x in range(52):
-    value=deck.cards[x].value
+def Hand_Value(player):
+  hand=player.hand
+  hand_value=0
+  for card in hand:
+    value=card.value
     if value in [2,3,4,5,6,7,8,9,10]: #If the card is 2, 3, 4, 5, 6, 7, 8, 9, 10, then the card value is it's current value.
-      deck.cards[x].value=value
+      hand_value=hand_value + value
     elif value in [11,12,13]: #If the card is a King, Queen, or Jack, then the card value is switched from its current value to 10.
-      deck.cards[x].value=10
+      hand_value=hand_value + 10
+    else:
+      if hand_value < 11:
+        hand_value=hand_value+11
+      else:
+        hand_value=hand_value+1
+  return hand_value
 
-def ace_value(Sum, card):
-  if Sum < 11:
-    card.value=11
-  else:
-    card.value=1
-  return card.value
