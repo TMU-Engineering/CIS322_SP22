d40f303adbfe5201f9f0247f1c895f4b7562018a
Ryan R <rastguelenianrn@masters.edu>
2022-03-20 16:34:48 -0700

    [Fixed] Card Match function for Bank

--- test/testing_base.py
+++ test/testing_base.py
@@ -1,4 +1,7 @@
 import sys
-sys.path.append("../")
+#sys.path.append("../")
+
+#only absolute path works for me? (Chris)
+sys.path.append("C:/Users/Chris/SoftwareEng/CIS322_SP22")
 
 from source.CardGames import *
