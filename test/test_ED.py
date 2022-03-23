688685b6b198efaab9958fea266d3401ba10a646
Joanna Johnson <johnsonjs1@masters.edu>
2022-03-22 20:41:02 -0700

    trial 1

---
+++ test/test_ED.py
@@ -0,0 +1,6 @@
+from testing_base import *
+
+sampleDeck = Deck()
+print(Deck)
+emptyD = sampleDeck.checkEmptyDeck()
+print(emptyD)
--- test/testing_base.py
+++ test/testing_base.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append("../")
+sys.path.append("C:/Users/joann/Desktop/Code/CIS322_SP22")
 from source.CardGames import *
 
 
