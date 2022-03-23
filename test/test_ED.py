a53baa3fc8a616a05ab079e1c7b6adb3978e61a7
Joanna Johnson <johnsonjs1@masters.edu>
2022-03-22 21:06:40 -0700

    final update

--- test/test_ED.py
+++ test/test_ED.py
@@ -1,6 +1,14 @@
 from testing_base import *
 
 sampleDeck = Deck()
-print(Deck)
-emptyD = sampleDeck.checkEmptyDeck()
-print(emptyD)
+def testCardlen():
+    while sampleDeck.size > 0:
+        emptyD = sampleDeck.checkEmptyDeck()
+        print(emptyD)
+        var = sampleDeck.getCard()
+
+    if sampleDeck.size == 0:
+        emptyD = sampleDeck.checkEmptyDeck()
+        print(emptyD)
+
+testCardlen()
