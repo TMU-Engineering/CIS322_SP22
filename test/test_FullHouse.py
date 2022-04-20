e24670bf49cf1678fed156a40a3651242cadd9c7
Jonathan Herren <herrenjm@masters.edu>
2022-04-19 20:46:12 -0700

    Fixed Find_FullHouse() and added another case for the test.

--- test/test_FullHouse.py
+++ test/test_FullHouse.py
@@ -1,6 +1,7 @@
 from testing_base import *
 
 def test_FullHouse():
+    # ---- Setup ---------------
     deckUsed = CardGames.Deck()
     deckUsed.shuffle()
     testPlayer = CardGames.Player("Test")
@@ -21,6 +22,50 @@ def test_FullHouse():
     riverPlayer.hand.append(h8)
     riverPlayer.hand.append(h5)
     riverPlayer.hand.append(s8)
+    # --------------------------
 
+
+
+    # ---- Execution -----------
     print(findFullHouse(testPlayer, riverPlayer))
-    assert(findFullHouse(testPlayer, riverPlayer) == (True, 5, 8))
+    isTrue = findFullHouse(testPlayer, riverPlayer)
+
+
+    # ---- Assertion -----------
+    assert(isTrue == True)
+
+
+def test_NotFullHouse():
+    # ---- Setup ---------------
+    deckUsed = CardGames.Deck()
+    deckUsed.shuffle()
+    testPlayer = CardGames.Player("Test")
+    riverPlayer = CardGames.Player("River")
+
+    s5 = deckUsed.findCard(6, "Spades")
+    c5 = deckUsed.findCard(5, "Clubs")
+    s10 = deckUsed.findCard(10, "Spades")
+    d5 = deckUsed.findCard(3, "Diamonds")
+    h8 = deckUsed.findCard(8, "Hearts")
+    h5 = deckUsed.findCard(5, "Hearts")
+    s8 = deckUsed.findCard(8, "Spades")
+
+    testPlayer.hand.append(s5)
+    testPlayer.hand.append(c5)
+    riverPlayer.hand.append(s10)
+    riverPlayer.hand.append(d5)
+    riverPlayer.hand.append(h8)
+    riverPlayer.hand.append(h5)
+    riverPlayer.hand.append(s8)
+    # --------------------------
+
+    
+
+    # ---- Execution -----------
+    print(findFullHouse(testPlayer, riverPlayer))
+    isTrue = findFullHouse(testPlayer, riverPlayer)
+
+
+    # ---- Assertion -----------
+    assert(isTrue == False)
+
